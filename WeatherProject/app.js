dd7b63146aceb29b8f8a049f1aba40272aae191d
Dom Henderson <dominic.henderson1@outlook.com>
2021-11-10 14:06:06 +0000

    Added weather report functionality

--- WeatherProject/app.js
+++ WeatherProject/app.js
@@ -1,10 +1,25 @@
 import express from 'express';
+import https from 'https';
+import { weatherApiKey } from './secret/apiKey.js';
 
 const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
 	res.send("This is the root of the website");
+
+	const baseWeatherUrl = 'https://api.openweathermap.org/data/2.5/weather';
+	const cityName = 'London';
+	const fullWeatherUrl = `${baseWeatherUrl}?q=${cityName}&appid=${weatherApiKey}`;
+
+	https.get(fullWeatherUrl, (weatherResponse) => {
+		console.log(weatherResponse.statusCode);
+
+		weatherResponse.on('data', (weatherData) => {
+			const parsedData = JSON.parse(weatherData);
+			console.log(parsedData);
+		})
+	})
 })
 
 app.listen(port, () => {
